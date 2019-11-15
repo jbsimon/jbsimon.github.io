e2cd296edb15e6d77177f62b92f6ac30aebd60fc
Jake Simon <jbsimon.astro@gmail.com>
2019-11-15 16:52:17 -0600

    11-15-19

---
+++ wafx_res/generated/wafxCommonPhpPage.php
@@ -0,0 +1,11 @@
+<?php
+$global_webacappella_auth_chain=[
+'auth_chain'=>[
+
+],'unid'=>'75532be3_57e6_4eb7_a568_f254537d6d4e'
+];
+$global_webacappella_messages=[
+'fr'=>[
+'limitedaccess:error authentication failed'=>'Erreur d\'authentification, verifiez l\'identifiant et le mot de passe','limitedaccess:feature not avalaible'=>'Cette fonctionnalité de restriction d\'accés n\'est pas disponible avec cette licence','limitedaccess:log button label'=>'Envoyer','limitedaccess:login placeholder'=>'Identifiant','limitedaccess:password placeholder'=>'Mot de passe','limitedaccess:welcome message'=>'Merci de vous authentifier afin d\'accéder à cette page'
+]
+];
