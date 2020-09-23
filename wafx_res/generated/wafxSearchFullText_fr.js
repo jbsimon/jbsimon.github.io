74117470eb0edb92d2c93a346ccc4083c95eddcc
jbsimon <jbsimon.astro@gmail.com>
2020-09-23 17:27:27 -0500

    09-23-20

---
+++ wafx_res/generated/wafxSearchFullText_fr.js
@@ -0,0 +1,83 @@
+ { 
+"map_targets":[
+ {  "title":"Jake Simon - Home" , "display_url":"index.html" , "url":"index.html" } 
+ , {  "title":"Page sans titre" , "display_url":"06a20866_add6_408c_91d3_41eaf6db730c.html" , "url":"06a20866_add6_408c_91d3_41eaf6db730c.html" } 
+ , {  "title":" Simon Research Group" , "display_url":"17b21a37_bb6b_4ee9_bbbd_5d018d0c5f43.html" , "url":"17b21a37_bb6b_4ee9_bbbd_5d018d0c5f43.html" } 
+ , {  "title":"Page sans titre" , "display_url":"2abe6115_26e3_4a73_a688_09ab4ea3ab71.html" , "url":"2abe6115_26e3_4a73_a688_09ab4ea3ab71.html" } 
+ , {  "title":"Page sans titre" , "display_url":"5fb0f8f4_0c8f_4f88_b587_d4d60922f41b.html" , "url":"5fb0f8f4_0c8f_4f88_b587_d4d60922f41b.html" } 
+ , {  "title":"Page sans titre" , "display_url":"66f635d1_84f3_45ba_a3ba_b1876e331122.html" , "url":"66f635d1_84f3_45ba_a3ba_b1876e331122.html" } 
+ , {  "title":"Jake Simon - Curriculum Vitae" , "display_url":"6879c75c_711a_48d0_b4b7_c7118d12a47b.html" , "url":"6879c75c_711a_48d0_b4b7_c7118d12a47b.html" } 
+ , {  "title":"Page sans titre" , "display_url":"6a582815_4a05_43b9_8ed3_1c21d67ca878.html" , "url":"6a582815_4a05_43b9_8ed3_1c21d67ca878.html" } 
+ , {  "title":"Page sans titre" , "display_url":"789195bb_a6c6_456c_9f98_64c86ebf0329.html" , "url":"789195bb_a6c6_456c_9f98_64c86ebf0329.html" } 
+ , {  "title":"Page sans titre" , "display_url":"83c3dcf5_873b_49da_b1a1_3d951a9fee1d.html" , "url":"83c3dcf5_873b_49da_b1a1_3d951a9fee1d.html" } 
+ , {  "title":"Jake Simon - Teaching" , "display_url":"940853ca_e0c3_4d00_bbf4_478dcd9503d5.html" , "url":"940853ca_e0c3_4d00_bbf4_478dcd9503d5.html" } 
+ , {  "title":"Page sans titre" , "display_url":"95c24eda_e362_4553_9b56_01d023c5876d.html" , "url":"95c24eda_e362_4553_9b56_01d023c5876d.html" } 
+ , {  "title":"Jake Simon - Research Topics" , "display_url":"ab92c95e_4926_48ab_a274_bd8cef2f7a58.html" , "url":"ab92c95e_4926_48ab_a274_bd8cef2f7a58.html" } 
+ , {  "title":"Jake Simon - Publications" , "display_url":"b2c98e7e_12eb_45a6_ab75_1d057d3b1c25.html" , "url":"b2c98e7e_12eb_45a6_ab75_1d057d3b1c25.html" } 
+ , {  "title":"Page sans titre" , "display_url":"d04b5cfa_0301_4db3_80ee_cf541859ee38.html" , "url":"d04b5cfa_0301_4db3_80ee_cf541859ee38.html" } 
+ , {  "title":"Page sans titre" , "display_url":"ecb4f2da_4631_405d_a3ed_b6e9716037d0.html" , "url":"ecb4f2da_4631_405d_a3ed_b6e9716037d0.html" } 
+],
+"map_words":["","2d","3d","also","auddy","be","bow","collapse","come","curriculum","disk","disks","effects","evolution","graduate","gravitating","gravitational","group","helmholtz","home","in","incoming","induced","instability","jake","jeonghoon","joing","joining","kelvin","lim","magnetic","magnetorotational","members","more","movies","my","of","postdoc","protoplanetary","publications","rayleigh","research","sayantan","self","shock","simon","soon","streaming","student","study","taylor","teaching","the","to","topics","tostudy","turbulence","under","vitae","will"]
+,
+"index_words":[ [14,9,8,11,10,5,4,7,6,1,0,3,2,13,12,15 ] 
+, [14,9,8,11,10,5,4,7,6,1,0,3,2,13,12,15 ] 
+, [14,9,8,11,10,5,4,7,6,1,0,3,2,13,12,15 ] 
+, [2 ] 
+, [2 ] 
+, [2 ] 
+, [14,9,8,11,10,5,4,7,6,1,0,3,2,13,12,15 ] 
+, [14,9,8,11,10,5,4,7,6,1,0,3,2,13,12,15 ] 
+, [2 ] 
+, [14,9,8,11,10,5,4,7,6,1,0,3,2,13,12,15 ] 
+, [14,9,8,11,10,5,4,7,6,1,0,3,2,13,12,15 ] 
+, [2 ] 
+, [2 ] 
+, [2 ] 
+, [2 ] 
+, [14,9,8,11,10,5,4,7,6,1,0,3,2,13,12,15 ] 
+, [14,9,8,11,10,5,4,7,6,1,0,3,2,13,12,15 ] 
+, [14,9,8,11,10,5,4,7,6,1,0,3,2,13,12,15 ] 
+, [14,9,8,11,10,5,4,7,6,1,0,3,2,13,12,15 ] 
+, [14,9,8,11,10,5,4,7,6,1,0,3,2,13,12,15 ] 
+, [2 ] 
+, [2 ] 
+, [14,9,8,11,10,5,4,7,6,1,0,3,2,13,12,15 ] 
+, [14,9,8,11,10,5,4,7,6,1,0,3,2,13,12,15 ] 
+, [10,6,0,13,12 ] 
+, [2 ] 
+, [2 ] 
+, [2 ] 
+, [14,9,8,11,10,5,4,7,6,1,0,3,2,13,12,15 ] 
+, [2 ] 
+, [2 ] 
+, [14,9,8,11,10,5,4,7,6,1,0,3,2,13,12,15 ] 
+, [2 ] 
+, [2 ] 
+, [14,9,8,11,10,5,4,7,6,1,0,3,2,13,12,15 ] 
+, [2 ] 
+, [2 ] 
+, [2 ] 
+, [2 ] 
+, [14,9,8,11,10,5,4,7,6,1,0,3,2,13,12,15 ] 
+, [14,9,8,11,10,5,4,7,6,1,0,3,2,13,12,15 ] 
+, [14,9,8,11,10,5,4,7,6,1,0,3,2,13,12,15 ] 
+, [2 ] 
+, [14,9,8,11,10,5,4,7,6,1,0,3,2,13,12,15 ] 
+, [14,9,8,11,10,5,4,7,6,1,0,3,2,13,12,15 ] 
+, [10,6,0,2,13,12 ] 
+, [2 ] 
+, [14,9,8,11,10,5,4,7,6,1,0,3,2,13,12,15 ] 
+, [2 ] 
+, [2 ] 
+, [14,9,8,11,10,5,4,7,6,1,0,3,2,13,12,15 ] 
+, [14,9,8,11,10,5,4,7,6,1,0,3,2,13,12,15 ] 
+, [2 ] 
+, [2 ] 
+, [12 ] 
+, [2 ] 
+, [14,9,8,11,10,5,4,7,6,1,0,3,2,13,12,15 ] 
+, [2 ] 
+, [14,9,8,11,10,5,4,7,6,1,0,3,2,13,12,15 ] 
+, [2 ] 
+]
+ } 
